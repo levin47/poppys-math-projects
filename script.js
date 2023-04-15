bd608708c6c7cc10e8f3faac80ede44557fef982
levin <124322728+levin47@users.noreply.github.com>
2023-04-14 20:07:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+const input = document.querySelector('input');
+const games = document.querySelectorAll('#games img');
+
+input.addEventListener('input', () => {
+  const searchTerm = input.value.toLowerCase();
+  games.forEach(game => {
+    if (game.alt.toLowerCase().includes(searchTerm)) {
+      game.style.display = 'block';
+    } else {
+      game.style.display = 'none';
+    }
+  });
+});
